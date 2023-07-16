e000e96d6f15032aae78fd3269b6eb73c87607ed
dugga <dugga@MSI>
2023-07-16 12:58:53 -0700

    Finished both of these almost

--- src/_03_gui_with_help/_1_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_03_gui_with_help/_1_my_first_swing_gui/MyFirstSwingGUI.java
@@ -39,7 +39,7 @@ frame.pack();
 		// 11. Run your program again. Do you see your message.
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
-
+label.setIcon(loadImage() );
 		// 13. Re-pack the JFrame object.
 frame.pack();
 		// 14. Run the program one more time. Do you see the image?
--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,32 +26,45 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+     String League1 = "https://www.jointheleague.org/wp-content/uploads/2022/01/logo2.png";
 		// 2. create a variable of type "Component" that will hold your image
-
+     Component One = createImage(League1);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
-
+     quizWindow.add(One);
 		// 5. call the pack() method on the quiz window
-
+     quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+     String Question1 = JOptionPane.showInputDialog("What color is the logo of the image?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+     if(Question1 == "Orange") {
+    	 System.out.println("CORRECT");
+     }
+     else {
+    	 System.out.println("INCORRECT");
+     }
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(One);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String League2 = "https://unblast.com/wp-content/uploads/2021/01/Space-Background-Image-2.jpg";
+Component two = createImage(League2);
 		// 11. add the second image to the quiz window
-
+quizWindow.add(two);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
+String Question2 = JOptionPane.showInputDialog("What is the color of this new image?");
+if(Question2 == "Blue") {
+	System.out.println("CORRECT");
+}
+else {
+	System.out.println("Incorrect");
+}
 
 		// 14+ check answer, say if correct or incorrect, etc.
 
